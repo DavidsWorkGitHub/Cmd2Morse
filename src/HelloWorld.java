93d8d0a28a03e23294009b752eaa06f6ff478623
Dane Griffiths <dane.griffiths2@dvla.gsi.gov.uk>
2016-08-30 14:58:09 +0100

    Refactored Hello World class, and added a translatory class.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,24 +1,51 @@
 public class HelloWorld{
+
     public static void main(String[] args) {
 
-        String morse[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..",
-                "--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",
-                "|",".---","..---","...--","....-",".....","-....","--...","---..","----.","-----"};
+        String[] text = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                        "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890";
+        String morse[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+                        "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
+                        "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
 
-        for (int i = 0; i < args.length; i++) {
+        CharacterMorseCode[] arrayOfChars = new CharacterMorseCode[text.length];
 
-            System.out.print(args[i]);
+        for (int i = 0; i < text.length; i++) {
+
+            arrayOfChars[i] = new CharacterMorseCode(text[i], morse[i]);
+
         }
-        for (String s: args) {
-            System.out.print(" " +s);
-        }
 
+        System.out.println("You entered " + args[0] + ".\n");
+        System.out.println("**********\nTranslated\n**********");
+        translateCharsToMorse(args[0], arrayOfChars);
 
-
-
+        //System.out.println(array[(translateCharsToMorse(args[0], array))].character + " = " +
+        //                      array[(translateCharsToMorse(args[0], array))].morseCode);
     }
 
+    /**
+     * Method to translate the text input in terminal into morse code.
+     * @param args A string containing the text entered in terminal.
+     * @param arrayOfTranslations An array containing the characters, and their retrospective morse code.
+     */
+    private static void translateCharsToMorse(String args, CharacterMorseCode[] arrayOfTranslations) {
 
+        // Loop through each iteration of the terminal text.
+        for (int i = 0; i < args.length(); i++) {
+
+            String current = String.valueOf(args.charAt(i));
+
+            // Loop through the array of characters to find the translated morse code.
+            for (CharacterMorseCode arrayOfTranslation : arrayOfTranslations) {
+
+                if (current.equalsIgnoreCase(arrayOfTranslation.character)) {
+
+                    System.out.println(arrayOfTranslation.character + " = " + arrayOfTranslation.morseCode);
+
+                }
+            }
+        }
+    }
 }
