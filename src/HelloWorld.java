d0b37156c8f4a413fe05503dfdbc247fbf2f3f67
Dane Griffiths <dane.griffiths2@dvla.gsi.gov.uk>
2016-09-05 11:42:40 +0100

    Changes to HelloWorld to incorporate the new Translator Factory class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 import translators.*;
 
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class HelloWorld {
 
@@ -9,7 +10,9 @@ public class HelloWorld {
         int parsedIndex = Integer.parseInt(arg2);
 
         Translator a = new MorseTranslator();
+
         System.out.println("The text to be translated into Morse Code: " + arg1);
+        System.out.println("Thadsfasdf: " + a.getSourceText());
         System.out.println("The translated text to Morse Code: " + a.translate(arg1));
         System.out.println("The morse code for character " + arg2 + ": " + a.getTranslatedCharacterAt(parsedIndex) + "\n");
 
@@ -22,6 +25,20 @@ public class HelloWorld {
         System.out.println("The text to be translated to lower case: " + arg1);
         System.out.println("The translated text to lower case: " + c.translate(arg1));
         System.out.println("The lower case for character " + arg2 + ": " + c.getTranslatedCharacterAt(parsedIndex) + "\n");
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Please enter the characters to be translated: ");
+        String inputText = s.nextLine();
+        System.out.println("Please enter the index point which you would like to be translated: ");
+        int inputIndex = Integer.parseInt(s.nextLine());
+        System.out.println("Please enter the type of translator you wish to use: ");
+        String inputTranslator = s.nextLine();
+
+        Translator z = new TranslatorFactory().getTranslatorFactory(inputTranslator);
+        System.out.println("\nThe text to be translated to lower case: " + inputText);
+        System.out.println("The translated text to lower case: " + z.translate(inputText));
+        System.out.println("The lower case for character " + inputIndex + ": " + z.getTranslatedCharacterAt(inputIndex) + "\n");
 
     }
 
