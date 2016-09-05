6c0a7b769f446a91701651aa24fe0fbc5db767d9
Dane Griffiths <dane.griffiths2@dvla.gsi.gov.uk>
2016-09-05 14:06:48 +0100

    Created Translator Factory, and updated main program to reflect factory update

--- src/translators/TranslatorFactory.java
+++ src/translators/TranslatorFactory.java
@@ -1,9 +1,37 @@
 package translators;
 
 /**
- * Method to .......
+ * Factory with enumerated type for correct translator to be selected.
  * Author: Dane Griffiths
  * Date created: 05/09/16.
  */
 public class TranslatorFactory {
+
+    public enum TranslatorSelect {MORSE,UPPERCASE,LOWERCASE}
+
+    /**
+     * Method to turn the users choice into the relevant translator.
+     * @param option
+     * @return
+     */
+    public Translator getTranslatorFactory(TranslatorSelect option) {
+
+        switch (option) {
+
+            case MORSE:
+                return new MorseTranslator();
+
+
+            case UPPERCASE:
+                return new UpperCaseTranslator();
+
+
+            case LOWERCASE:
+                return new LowerCaseTranslator();
+
+            default:
+                return null;
+
+        }
+    }
 }
