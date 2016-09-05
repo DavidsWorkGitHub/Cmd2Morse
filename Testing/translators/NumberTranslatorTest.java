3d9bb8872647cca11fea6c63304b9a37a3034277
David Powell <dai_powell@me.com>
2016-09-05 15:44:34 +0100

    Updated Number Translator Test

--- Testing/translators/NumberTranslatorTest.java
+++ Testing/translators/NumberTranslatorTest.java
@@ -3,10 +3,8 @@ package translators;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
-<<<<<<< Updated upstream
-import static org.junit.Assert.*;
-=======
->>>>>>> Stashed changes
+
+
 
 /**
  * Created by david on 05/09/16.
@@ -20,29 +18,19 @@ public class NumberTranslatorTest {
 
         assertEquals("abc One  Two  Three  Four  Five  Six  Seven  Eight  Nine  Zero ", d.translate(text));
     }
-<<<<<<< Updated upstream
+
     @Test
     public void getSourceText() throws Exception {
 
 
         Translator d = new NumberTranslator();
         String text = "abc1234567890";
-=======
 
-    @Test
-    public void getSourceText() throws Exception {
-
-        Translator d = new NumberTranslator();
-        String text = "abc One  Two  Three  Four  Five  Six  Seven  Eight  Nine  Zero ";
->>>>>>> Stashed changes
         d.translate(text);
 
         assertEquals(text, d.getSourceText());
     }
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
     @Test
     public void getTranslatedCharacterAt() throws Exception {
 
@@ -50,15 +38,9 @@ public class NumberTranslatorTest {
         String text = "abc1234567890";
         d.translate(text);
 
-<<<<<<< Updated upstream
         int index = 3;
         assertEquals("1" , d.getTranslatedCharacterAt(index));
     }
 
 }
-=======
-        int index = 2;
-        assertEquals("c", d.getTranslatedCharacterAt(index));
-    }
-}
->>>>>>> Stashed changes
+
